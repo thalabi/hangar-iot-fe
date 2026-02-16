b9000cac6e16b0d71040585294f770c49709e1b6
Tarif Halabi <tarif.halabi@gmail.com>
2026-02-15 20:15:58 -0500

    Converted device-list template to tailwind.

--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -5,17 +5,33 @@ import { RestService } from '../service/rest.service';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
     standalone: true,
     selector: 'app-page1',
-    imports: [CommonModule, FormsModule, TableModule],
+    imports: [CommonModule, FormsModule, TableModule, MultiSelectModule],
     templateUrl: './device-list.component.html',
     styleUrls: ['./device-list.component.css']
 })
 export class DeviceListComponent implements OnInit {
     deviceNameList: Array<string> = []
     deviceList: Array<Device> = []
+
+    allColumns = [
+        { field: 'name', header: 'Name' },
+        { field: 'make', header: 'Make' },
+        { field: 'model', header: 'Model' },
+        { field: 'deviceType', header: 'Type' },
+        { field: 'bridge', header: 'Bridge' },
+        { field: 'description', header: 'Description' },
+        { field: 'telemetry', header: 'Telemetry' },
+        { field: 'enableDataSaver', header: 'Data saver' },
+        { field: 'passive', header: 'Passive' },
+        { field: 'isManaged', header: 'Managed' },
+    ];
+    selectedColumns = this.allColumns; // Start with all visible
+
     constructor(
         private restService: RestService,
         private messageService: MessageService
